e18f47002d4f9e0b133322278393f8404bdedf49
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-06-20 13:06:12 +0200

    Fixed code for shortest string

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -14,11 +14,8 @@ function findShortestString(stringArr) {
       shortestString = str;
     }
   }
-  return shortestString.length;
+  return shortestString;
 }
 
 module.exports = findShortestString;
 
-console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
-console.log(findShortestString(["why", "hello", "to", "you"]), "to");
-console.log(findShortestString(["brave", "dance"]), "brave");
